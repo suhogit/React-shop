6de4fb7234000d4df9b92f50fde755471b94b735
suhogit <dltngh2236@naver.com>
2022-01-19 17:59:12 +0900

    styled-components를 이용한 class없는 CSS스타일링

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
   <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+  <Navbar.Brand><Link to="/">ShoeShop</Link></Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
