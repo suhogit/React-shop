0e5c0b5a7b8b7d19f0807f235b2ef0cf1804816c
suhogit <dltngh2236@naver.com>
2022-01-20 17:40:21 +0900

    useEffect.....악git add . 어려워

--- src/Detail.js
+++ src/Detail.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.scss';
@@ -13,7 +13,31 @@ let 제목 = styled.h4`
 `;
 
 
+// class Detail2 extends React.Component {
+
+//   componentDidMount(){
+
+//   }
+
+//   componentWillUnmount(){
+
+//   }
+
+// }
+
+
+
+
+
 function Detail (props) {
+
+  useEffect(()=>{
+    let 타이머 = setTimeout(()=>{ alert변경(false) }, 2000);
+
+    return ()=>{ clearTimeout(타이머) }
+  }, []);  //,[] 은 그냥 컴포넌트 로드때만 한번 딱 실행하고 싶은 코드를 담을 때 쓸 수 있는 일종의 트릭쇼입니다.
+
+  let [alert, alert변경] = useState(true);
 
     let {id} = useParams();
     let history = useHistory();
@@ -28,9 +52,17 @@ function Detail (props) {
             <제목 className="red">Detail</제목>
          </박스>
 
-         <div className="my-alert2">
-           <p>재고가 얼마 남지 않았습니다</p>
-         </div>
+
+         {
+           
+           alert === true 
+           ? (<div className="my-alert2">
+                <p>재고가 얼마 남지 않았습니다</p>
+              </div>)
+           : null
+
+         }
+
 
       <div className="row">
         <div className="col-md-6">
