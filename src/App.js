c7bb37834a2c35f205b3dacc621667555bace721
suhogit <dltngh2236@naver.com>
2022-01-27 12:15:06 +0900

    재택 테스트

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link as={Link} to="/">Home</Nav.Link>
+      <Nav.Link as={Link} to="/">Homeeeeeee</Nav.Link>
       <Nav.Link as={Link} to="/detail">Detail</Nav.Link>
       <NavDropdown title="Dropdown" id="basic-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
